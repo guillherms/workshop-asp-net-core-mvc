8791b66d1971bdfe93c72e076e7259d5a4f5fd2a
guillherms <guisantosoliv@gmail.com>
2019-07-25 00:16:39 -0300

    Create Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -12,15 +13,42 @@ namespace SalesWebMvc.Controllers
 
         private readonly SellerService _sellerService;
 
+        //private readonly SalesWebMvcContext _context;
+
+
         public SellersController(SellerService sellerService)
         {
             _sellerService = sellerService;
+            //context = _context;
         }
 
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        ////    public async Task<IActionResult> Create([Bind("Id, Name, Email, BirthDate, BaseSalary, Departament")] Seller seller)
+        ////    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            _context.Add(seller);
+        //            await _context.SaveChangesAsync();
+        //            /*return RedirectToAction(nameof(Index));*/
+        //        }
+        //        return View(seller);
+        //    }
+        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof (Index));
         }
     }
 }
